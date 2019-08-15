42a548220f53ffc0af8902b9de6424bc3f9bf047
razli.mahadi <razli1993@hotmail.com>
2019-08-15 14:56:31 +0800

    Basics of Javascript

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// /* A functional component (stateless) */
+// function App() {
+//   const greeting = 'Hello.. I am a functional component';
+//   return <h1>{greeting}</h1>
+// }
 
-export default App;
+// /* A Class component */
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello I am from class</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// /* App which returns JSX */
+// function App() {
+//   return <Headline />
+// }
+// function Headline() {
+//   const greeting = 'Im from the headline component!';
+//   return <h1>{greeting}</h1>
+// }
+
+// /* Passing props around */
+// function App() {
+//   const greeting = 'Props example';
+//   return <Headline value={greeting} />  // Same as Headline(greeting)
+// }
+// function Headline(props) {
+//   return <h1>{props.value}</h1>
+// }
+
+// const App = () => {
+//   const greeting = 'Arrow function';
+//   return <Headline value={greeting} />
+// };
+// const Headline = ({value}) => {   
+//   return <h1>{value}</h1>
+// };
+
+/* Nested function calls */
+const App = () => {
+  const greeting = 'Hello there!';
+  return <Headline value={greeting} />
+};
+const Headline = ({value}) => {   
+  return <Footer value={value}/>
+};
+const Footer = ({value}) => {
+  return <h1>{value}</h1>
+};
+
+// /* Calling 2 functional components together */
+// const App = () => {
+//   const greeting = 'Greeting';
+//   const goodbye = 'Goodbye';
+//   return (
+//     <div>
+//       <Headline value1={greeting} />
+//       <Footer value2={goodbye} />
+//     </div>
+//   ) 
+// };
+// const Headline = ({value1}) => {   
+//   return <h1>{value1}</h1>
+// };
+// const Footer = ({value2}) => {
+//   return <h2>{value2}</h2>
+// };
+
+export default App; /* Will return as a JSX file */ 
